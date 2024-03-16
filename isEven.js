6c590ce1dfa5bde3e809178d02569ea403fd4dfb
devlrakib <devrakib5@gmail.com>
2024-03-16 19:38:41 +0600

    first commit

---
+++ isEven.js
@@ -0,0 +1,12 @@
+function isEven(number){
+    const remainder = number % 2;
+    // console.log(remainder);
+    if  (remainder === 0){
+        console.log('This is Even Number');
+    }
+    else {
+        console.log('This is Odd Number');
+    }
+}
+isEven (7);
+
